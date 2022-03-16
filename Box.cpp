61ada5591b2ea95b29309b5761172f44d7db679a
it21204098 <99202324+it21204098@users.noreply.github.com>
2022-03-16 14:08:48 +0000

    Exercise two completed

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,29 @@
+#include<iostream>
 #include "Box.h"
+using namespace std;
+
 
 // Implement setters and getters
+void Box::setLength(int l){
+  length = l;
+}
+int Box::getLength(){
+  return length;
+}
+void Box::setWidth(int w){
+  width = w;
+}
+int Box::getWidth(){
+  return width;
+}
+void Box::setHeight(int h){
+  height = h;
+}
+int Box::getHeight(){
+  return height;
+}
 
 // Implemenet the calcVolume() unction
-int Box::calcVolume() {
+int Box::calcVolume(int l, int w, int h) {
+  return (l*w*h);
 }
--- Exercise02.cpp
+++ Exercise02.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-using namespace std;
 #include "Box.h"
+using namespace std;
 
 int main() {
   
    // 1. Create a Box type object called box1
+  Box box1;
   
   
    // ======= DO NOT CHANGE THE INPUT ===========
@@ -12,18 +13,21 @@ int main() {
    cout << "Enter the Height of the Box : ";
    cin >> height;
    cout << "Enter the Length of the Box : ";
-   cin >> length;
+   cin >>length;
    cout << "Enter the width of the Box : ";
    cin >> width;
    // ===========================================
   
    // 2. Use setters assign height, length, width throw
+  box1.setLength(length);
+  box1.setWidth(width);
+  box1.setHeight(height);
   
    // === DO NOT CHANGE THE OUTPUT =============
    cout << "Box Height " << box1.getHeight() << endl;
    cout << "Box Length " << box1.getLength() << endl;
    cout << "Box Width " << box1.getWidth() << endl;
-   cout << "Volume of Box is " << box1.calcVolume() << endl;
+   cout << "Volume of Box is " << box1.calcVolume(length,width,height) << endl;
    // ==========================================
       return 0;
 }
